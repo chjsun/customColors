c550fa07e7682e6947b7d29ad7583876486bf6a5
GitHub Action <action@github.com>
2022-07-25 00:53:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/chjsun/customcolors/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chjsun/customcolors/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chjsun.customcolors.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chjsun.customcolors.MyBundle
 
 class MyApplicationService {
 
